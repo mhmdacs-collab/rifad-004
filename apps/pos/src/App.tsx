625f4a232e35a331d376c05a97d9b431f8c0c8a7
Geo-Smart <mhmd.acs@gmail.com>
2026-08-17 01:29:10 +0300

    feat(pos): provide customer workflow to POS surfaces

--- apps/pos/src/App.tsx
+++ apps/pos/src/App.tsx
@@ -5,6 +5,7 @@ import { ReceiptsScreen } from "./screens/ReceiptsScreen";
 import { SalesScreen } from "./screens/SalesScreen";
 import { SignInScreen } from "./screens/SignInScreen";
 import { SuccessScreen } from "./screens/SuccessScreen";
+import { CustomerFlowProvider } from "./state/CustomerFlowContext";
 import { usePosFlow } from "./state/usePosFlow";
 
 export default function App() {
@@ -35,39 +36,48 @@ export default function App() {
 
   if (flow.stage === "sales" && flow.ticket) {
     return (
-      <SalesScreen
-        employee={flow.employee}
-        ticket={flow.ticket}
-        products={flow.products}
-        allProducts={flow.allProducts}
-        salePages={flow.salePages}
-        activePageId={flow.activePageId}
-        query={flow.query}
-        busy={flow.busy}
-        errorMessage={flow.errorMessage}
-        lastTouchedLineId={flow.lastTouchedLineId}
-        onDismissError={flow.clearError}
-        onQueryChange={flow.setQuery}
-        onPageChange={flow.setActivePageId}
-        onCreatePage={flow.createSalePage}
-        onRenamePage={flow.renameSalePage}
-        onDeletePage={flow.deleteSalePage}
-        onMovePage={flow.moveSalePage}
-        onPlacePageProduct={(pageId, slotIndex, productId) => void flow.placeSalePageProduct(pageId, slotIndex, productId)}
-        onRemovePageProduct={(pageId, slotIndex) => void flow.removeSalePageProduct(pageId, slotIndex)}
-        onAddProduct={(id) => void flow.addProduct(id)}
-        onSetQuantity={(id, value) => void flow.setQuantity(id, value)}
-        onRemoveLine={(id) => void flow.removeLine(id)}
-        onSaveTicket={() => void flow.saveOpenTicket()}
-        onCheckout={() => void flow.beginCheckout()}
-        onOpenReceipts={() => void flow.openReceipts()}
-        onSearchCustomers={flow.searchCustomers}
-        onCreateCustomer={flow.createCustomer}
-        onSetTicketCustomer={flow.setTicketCustomer}
-        onLoadCustomerLedger={flow.loadCustomerLedger}
-        onChargeCredit={flow.chargeTicketToCustomer}
-        onSettleDebt={flow.settleCustomerDebt}
-      />
+      <CustomerFlowProvider value={{
+        ticket: flow.ticket,
+        updateCustomer: flow.updateCustomer,
+        applyLoyaltyRedemption: flow.applyLoyaltyRedemption,
+        loadLoyaltyStatus: flow.loadLoyaltyStatus,
+        quoteLoyaltyRedemption: flow.quoteLoyaltyRedemption,
+        loadCustomerPurchases: flow.loadCustomerPurchases,
+      }}>
+        <SalesScreen
+          employee={flow.employee}
+          ticket={flow.ticket}
+          products={flow.products}
+          allProducts={flow.allProducts}
+          salePages={flow.salePages}
+          activePageId={flow.activePageId}
+          query={flow.query}
+          busy={flow.busy}
+          errorMessage={flow.errorMessage}
+          lastTouchedLineId={flow.lastTouchedLineId}
+          onDismissError={flow.clearError}
+          onQueryChange={flow.setQuery}
+          onPageChange={flow.setActivePageId}
+          onCreatePage={flow.createSalePage}
+          onRenamePage={flow.renameSalePage}
+          onDeletePage={flow.deleteSalePage}
+          onMovePage={flow.moveSalePage}
+          onPlacePageProduct={(pageId, slotIndex, productId) => void flow.placeSalePageProduct(pageId, slotIndex, productId)}
+          onRemovePageProduct={(pageId, slotIndex) => void flow.removeSalePageProduct(pageId, slotIndex)}
+          onAddProduct={(id) => void flow.addProduct(id)}
+          onSetQuantity={(id, value) => void flow.setQuantity(id, value)}
+          onRemoveLine={(id) => void flow.removeLine(id)}
+          onSaveTicket={() => void flow.saveOpenTicket()}
+          onCheckout={() => void flow.beginCheckout()}
+          onOpenReceipts={() => void flow.openReceipts()}
+          onSearchCustomers={flow.searchCustomers}
+          onCreateCustomer={flow.createCustomer}
+          onSetTicketCustomer={flow.setTicketCustomer}
+          onLoadCustomerLedger={flow.loadCustomerLedger}
+          onChargeCredit={flow.chargeTicketToCustomer}
+          onSettleDebt={flow.settleCustomerDebt}
+        />
+      </CustomerFlowProvider>
     );
   }
 
@@ -115,6 +125,7 @@ export default function App() {
         printStatus={flow.printStatus}
         busy={flow.busy !== null}
         onPrint={() => void flow.printReceipt()}
+        onEmailReceipt={flow.emailReceipt}
         onNewSale={() => void flow.newSale()}
       />
     );
