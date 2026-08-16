0a6b3b9de153e23ee5fbee2de463063654907636
Geo-Smart <mhmd.acs@gmail.com>
2026-08-16 19:26:19 +0300

    style(pos): load checkout ticket hierarchy pass

--- apps/pos/src/main.tsx
+++ apps/pos/src/main.tsx
@@ -10,6 +10,7 @@ import "./visual-pass-05.css";
 import "./visual-pass-06.css";
 import "./visual-pass-07.css";
 import "./visual-pass-08.css";
+import "./visual-pass-09.css";
 
 createRoot(document.getElementById("root")!).render(<StrictMode><App /></StrictMode>);
 
