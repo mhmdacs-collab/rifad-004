2e7f1cff77b0003d45e9527d0c1f9b581ec8ad80
Geo-Smart <mhmd.acs@gmail.com>
2026-08-16 23:15:12 +0300

    style(pos): load customer system styles

--- apps/pos/src/main.tsx
+++ apps/pos/src/main.tsx
@@ -16,6 +16,7 @@ import "./visual-pass-11.css";
 import "./visual-pass-12.css";
 import "./customer-credit.css";
 import "./customer-credit-partial.css";
+import "./customer-system.css";
 
 createRoot(document.getElementById("root")!).render(<StrictMode><App /></StrictMode>);
 
