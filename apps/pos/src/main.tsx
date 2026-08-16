2082b88ad6c11ee249837e2e5fab9c024ba50a19
Geo-Smart <mhmd.acs@gmail.com>
2026-08-17 02:50:24 +0300

    chore(pos): load application polish pass

--- apps/pos/src/main.tsx
+++ apps/pos/src/main.tsx
@@ -18,6 +18,7 @@ import "./customer-credit.css";
 import "./customer-credit-partial.css";
 import "./customer-system.css";
 import "./customer-loyalty.css";
+import "./visual-pass-13.css";
 
 createRoot(document.getElementById("root")!).render(<StrictMode><App /></StrictMode>);
 
