d6add1610d64296f4fe46fc6db7f61addcfc2a9e
Geo-Smart <mhmd.acs@gmail.com>
2026-08-16 21:04:58 +0300

    style(pos): load partial settlement UI styles

--- apps/pos/src/main.tsx
+++ apps/pos/src/main.tsx
@@ -15,6 +15,7 @@ import "./visual-pass-10.css";
 import "./visual-pass-11.css";
 import "./visual-pass-12.css";
 import "./customer-credit.css";
+import "./customer-credit-partial.css";
 
 createRoot(document.getElementById("root")!).render(<StrictMode><App /></StrictMode>);
 
