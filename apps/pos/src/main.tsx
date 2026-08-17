385b8a634c89f8967d596ce1effb76c5f0b4af3c
Geo-Smart <mhmd.acs@gmail.com>
2026-08-17 14:47:43 +0300

    style(pos): load cash hierarchy pass 40

--- apps/pos/src/main.tsx
+++ apps/pos/src/main.tsx
@@ -46,6 +46,7 @@ import "./visual-pass-36.css";
 import "./visual-pass-37.css";
 import "./visual-pass-38.css";
 import "./visual-pass-39.css";
+import "./visual-pass-40.css";
 
 installVisualPass18ProductPrices();
 
