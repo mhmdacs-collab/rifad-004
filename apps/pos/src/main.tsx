e1f33cde4efcb358a1d374c33a695d997e9b1501
Geo-Smart <mhmd.acs@gmail.com>
2026-08-17 03:41:41 +0300

    chore(pos): load product rhythm pass

--- apps/pos/src/main.tsx
+++ apps/pos/src/main.tsx
@@ -26,6 +26,7 @@ import "./visual-pass-16.css";
 import "./visual-pass-17.css";
 import "./visual-pass-18.css";
 import "./visual-pass-19.css";
+import "./visual-pass-20.css";
 
 installVisualPass18ProductPrices();
 
