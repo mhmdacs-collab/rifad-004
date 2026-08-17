7ffa79cf3c31aa85b2cbdb81e6c3e3e577004e36
Geo-Smart <mhmd.acs@gmail.com>
2026-08-17 05:40:37 +0300

    chore(pos): load touch-first responsive audit

--- apps/pos/src/main.tsx
+++ apps/pos/src/main.tsx
@@ -42,6 +42,7 @@ import "./visual-pass-32.css";
 import "./visual-pass-33.css";
 import "./visual-pass-34.css";
 import "./visual-pass-35.css";
+import "./visual-pass-36.css";
 
 installVisualPass18ProductPrices();
 
