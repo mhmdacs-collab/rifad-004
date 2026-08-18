d8dc0cc3dc26ee708833a8f5435f8a0dc2366fab
Geo-Smart <mhmd.acs@gmail.com>
2026-08-18 13:52:07 +0300

    fix(backoffice): resolve shared catalog test setup

--- apps/backoffice/src/test/setup.ts
+++ apps/backoffice/src/test/setup.ts
@@ -1,7 +1,7 @@
 import "@testing-library/jest-dom/vitest";
 import { cleanup } from "@testing-library/react";
 import { afterEach, beforeEach } from "vitest";
-import { BROWSER_CATALOG_STORAGE_KEY } from "../../../adapters/catalog/browserCatalog";
+import { BROWSER_CATALOG_STORAGE_KEY } from "../../../../adapters/catalog/browserCatalog";
 
 beforeEach(() => window.localStorage.removeItem(BROWSER_CATALOG_STORAGE_KEY));
 afterEach(() => cleanup());
