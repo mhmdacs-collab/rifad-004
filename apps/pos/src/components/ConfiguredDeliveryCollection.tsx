ab18f3b3615452827a682edb183c2108eca86401
Geo-Smart <mhmd.acs@gmail.com>
2026-08-20 01:54:21 +0300

    Fix delivery selector state import

--- apps/pos/src/components/ConfiguredDeliveryCollection.tsx
+++ apps/pos/src/components/ConfiguredDeliveryCollection.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import type { EffectiveDeliveryChannel, EffectiveDeliveryConfiguration } from "../../../../contracts/posConfiguration";
 
 const executableChannel = (channel: EffectiveDeliveryChannel) =>
